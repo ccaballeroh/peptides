c1f226356a02f013448adfb7e781909d2503a65f
ccaballeroh <cdch10@gmail.com>
2022-04-04 01:20:42 -0500

    new constants

--- src/constants.py
+++ src/constants.py
@@ -22,12 +22,19 @@ __all__ = [
     "DB_OUTPUT_FILE",
     "PASTA_INPUT_PATH",
     "MODIFICATIONS_RULES",
+    "swiss_args",
+    "ffncaa_args",
+    "swiss",
+    "ffncaa",
 ]
 
 BASE_URL = "https://webs.iiitd.edu.in/raghava/pepstrmod/"
 LETTERS_FIELDS = [f"aa{i}" for i in range(1, 26)]
 MODIFICATIONS_FIELDS = [f"secstr{i}" for i in range(1, 26)]
 _MODIFIERS_FILE = Path(r"support_files/modifiers.json")
+
+PATH = Path(r"support_files")
+
 
 if not _MODIFIERS_FILE.exists():
     modifiers_parser.main()
@@ -36,3 +43,8 @@ with _MODIFIERS_FILE.open("r") as f:
     CONVERSION = json.load(f)
 
 DB_OUTPUT_PATH = Path(r".")
+
+swiss_args = modifiers_parser.swiss_args
+ffncaa_args = modifiers_parser.ffncaa_args
+swiss = modifiers_parser.swiss
+ffncaa = modifiers_parser.ffncaa
